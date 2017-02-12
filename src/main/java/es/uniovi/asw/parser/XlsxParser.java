e11eabcae108f08bc8f1acdae25ce434e860ce12
Carla <carla@pitando.com>
2017-02-12 18:48:10 +0100

    Added polling station to code and tests

--- src/main/java/es/uniovi/asw/parser/XlsxParser.java
+++ src/main/java/es/uniovi/asw/parser/XlsxParser.java
@@ -1,74 +1,86 @@
 package es.uniovi.asw.parser;
 
-import es.uniovi.asw.RandomPasswordGenerator;
-import es.uniovi.asw.Citizen;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.commons.lang.RandomStringUtils;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import es.uniovi.asw.Citizen;
+import es.uniovi.asw.letters.SendLetters;
 
 /**
  * Created by Carla on 08/02/2017.
  */
 public class XlsxParser implements Parser {
-    @Override
-    public List<Citizen> parseFile(File file) {
-        List users = new ArrayList<Citizen>();
+	SendLetters letter = new SendLetters();
 
-        try {
-            XSSFWorkbook wb = new XSSFWorkbook(new FileInputStream(file));
-            XSSFSheet sheet = wb.getSheetAt(0);
-            XSSFRow row;
+	@Override
+	public List<Citizen> parseFile(File file) {
+		List<Citizen> users = new ArrayList<Citizen>();
 
-            int rows; // No of rows
-            rows = sheet.getPhysicalNumberOfRows();
+		try {
+			XSSFWorkbook wb = new XSSFWorkbook(new FileInputStream(file));
+			XSSFSheet sheet = wb.getSheetAt(0);
+			XSSFRow row;
 
-            int cols = 0; // No of columns
-            int tmp = 0;
+			int rows; // No of rows
+			rows = sheet.getPhysicalNumberOfRows();
 
-            // This trick ensures that we get the data properly even if it doesn't start from first few rows
-            for (int i = 0; i < 10 || i < rows; i++) {
-                row = sheet.getRow(i);
-                if (row != null) {
-                    tmp = sheet.getRow(i).getPhysicalNumberOfCells();
-                    if (tmp > cols) cols = tmp;
-                }
-            }
+			int cols = 0; // No of columns
+			int tmp = 0;
 
-            parseUsers(users, sheet, rows);
+			// This trick ensures that we get the data properly even if it
+			// doesn't start from first few rows
+			for (int i = 0; i < 10 || i < rows; i++) {
+				row = sheet.getRow(i);
+				if (row != null) {
+					tmp = sheet.getRow(i).getPhysicalNumberOfCells();
+					if (tmp > cols)
+						cols = tmp;
+				}
+			}
 
+			parseUsers(users, sheet, rows);
+			wb.close();
 
-        } catch (Exception ioe) {
-            ioe.printStackTrace();
-        }
+		} catch (Exception ioe) {
+			ioe.printStackTrace();
+		}
 
-        return users;
+		return users;
 
-    }
+	}
 
-    private void parseUsers(List users, XSSFSheet sheet, int rows) {
-        XSSFRow row;// se salta la primera línea
-        for (int r = 1; r < rows; r++) {
-            row = sheet.getRow(r);
-            if (row != null) {
-                String nombre = row.getCell(0).getStringCellValue();
-                String apellidos = row.getCell(1).getStringCellValue();
-                String email = row.getCell(2).getStringCellValue();
-                Date nacimiento = row.getCell(3).getDateCellValue();
-                String direccion = row.getCell(4).getStringCellValue();
-                String nacionalidad = row.getCell(5).getStringCellValue();
-                String dni = row.getCell(6).getStringCellValue();
+	private void parseUsers(List<Citizen> users, XSSFSheet sheet, int rows) throws IOException {
+		XSSFRow row;// se salta la primera línea
+		for (int r = 1; r < rows; r++) {
+			row = sheet.getRow(r);
+			if (row != null) {
+				String nombre = row.getCell(0).getStringCellValue();
+				String apellidos = row.getCell(1).getStringCellValue();
+				String email = row.getCell(2).getStringCellValue();
+				Date nacimiento = row.getCell(3).getDateCellValue();
+				String direccion = row.getCell(4).getStringCellValue();
+				String nacionalidad = row.getCell(5).getStringCellValue();
+				String dni = row.getCell(6).getStringCellValue();
+				int polling = (int) row.getCell(7).getNumericCellValue();
 
-                Citizen user = new Citizen(dni, nombre, apellidos, nacimiento, direccion, email, nacionalidad);
-                user.setPassword(RandomPasswordGenerator.generatePassword());
+				Citizen citizen = new Citizen(dni, nombre, apellidos, nacimiento, direccion, email, nacionalidad, polling);
 
-                users.add(user);
-            }
-        }
-    }
+				// create a random alphanumeric password and persists the user
+				citizen.setPassword(RandomStringUtils.randomAlphanumeric(10));
+				
+				letter.send(citizen);
+
+				users.add(citizen);
+			}
+		}
+	}
 }
--- src/test/java/es/uniovi/asw/parser/XlsxParserTest.java
+++ src/test/java/es/uniovi/asw/parser/XlsxParserTest.java
@@ -1,13 +1,13 @@
 package es.uniovi.asw.parser;
 
-import es.uniovi.asw.Citizen;
-import es.uniovi.asw.User;
-import org.junit.*;
+import static org.junit.Assert.assertEquals;
 
 import java.io.File;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+import es.uniovi.asw.Citizen;
 
 /**
  * Created by Carla on 08/02/2017.
@@ -20,22 +20,22 @@ public class XlsxParserTest {
         File file = new File("src/test/resources/testSmall.xlsx");
         List<Citizen> users = parser.parseFile(file);
 
-        assertEquals("Nombre: Juan; Apellidos: Torres Pardo; " +
-                "Email: juan@example.com; Nacimiento: 10/10/1985; " +
-                "Dirección: C/ Federico García Lorca 2; Nacionalidad: " +
-                "Español; DNI: 90500084Y",
+        assertEquals("Name: Juan; Surname: Torres Pardo; " +
+                "Email: juan@example.com; Birth date: 10/10/1985; " +
+                "Address: C/ Federico García Lorca 2; Nationality: " +
+                "Español; DNI: 90500084Y; Polling station: 1",
                 users.get(0).toString());
 
-        assertEquals("Nombre: Luis; Apellidos: López Fernando; " +
-                        "Email: luis@example.com; Nacimiento: 02/03/1970; " +
-                        "Dirección: C/ Real Oviedo 2; Nacionalidad: " +
-                        "Español; DNI: 19160962F",
+        assertEquals("Name: Luis; Surname: López Fernando; " +
+                        "Email: luis@example.com; Birth date: 02/03/1970; " +
+                        "Address: C/ Real Oviedo 2; Nationality: " +
+                        "Español; DNI: 19160962F; Polling station: 2",
                 users.get(1).toString());
 
-        assertEquals("Nombre: Ana; Apellidos: Torres Pardo; " +
-                        "Email: ana@example.com; Nacimiento: 01/01/1960; " +
-                        "Dirección: Av. De la Constitución 8; Nacionalidad: " +
-                        "Español; DNI: 09940449X",
+        assertEquals("Name: Ana; Surname: Torres Pardo; " +
+                        "Email: ana@example.com; Birth date: 01/01/1960; " +
+                        "Address: Av. De la Constitución 8; Nationality: " +
+                        "Español; DNI: 09940449X; Polling station: 3",
                 users.get(2).toString());
     }
 
